ec0b3481a2d93b01cc243733f03a3fa6aedb5aef
Joseph Ramsay <jramsay@linz.govt.nz>
2017-02-01 15:17:55 +1300

    CI23

--- test/LayerReader_Test.py
+++ test/LayerReader_Test.py
@@ -23,6 +23,7 @@ import inspect
 import sys
 import re
 import os
+import yaml
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'../src')))
 
@@ -32,10 +33,19 @@ from LayerReader import LayerReader, initds, SFDS, PGDS
 
 testlog = Logger.setup('test')
 PGCONNSTR = "PG: dbname='{d}' host='{h}' port='{p}' user='{u}' password='{x}' active_schema={s}"
-#1:['127.0.0.1','template1',5432,'postgres','password','public'],
-PP = {1:['127.0.0.1','test_db',5432,'test_user','test_pass','public'],
-      2:['127.0.0.1','test_db',5432,'test_user','test_pass','public']}
-P = PP[1 if os.getenv('TRAVIS') else 2]
+
+def config():
+    #1:['127.0.0.1','template1',5432,'postgres','password','public'],
+    PP = {1:{'host':'127.0.0.1','database':'','port':5432,'username':'','password':'','schema':'public'},
+          2:{'host':'127.0.0.1','database':'','port':5432,'username':'','password':'','schema':'public'}}
+    P = PP[1 if os.getenv('TRAVIS') else 2]
+    
+    
+    with open(os.path.join(os.path.dirname(__file__),"database.yml"), 'r') as dby:
+        try:
+            P.update(yaml.load(dby)['postgres'])
+        except yaml.YAMLError as exc:
+            print(exc)
 
 
 class Test_0_LayerReaderSelfTest(unittest.TestCase):
@@ -60,7 +70,7 @@ class Test_1_LayerReaderConfigTest(unittest.TestCase):
     '''Test LayerReader functions'''
         
     def setUp(self):
-        pgds = PGDS(PGCONNSTR.format(h=P[0],d=P[1],p=P[2],u=P[3],x=P[4],s=P[5]))
+        pgds = PGDS(PGCONNSTR.format(h=P['host'],d=P['database'],p=P['port'],u=P['username'],x=P['password'],s=P['schema']))
         self.layerreader = LayerReader(initds(SFDS,'CropRegions.shp'),pgds)
         
     def tearDown(self):
